aedacab7c943fb5552083ba1bfb0f941e32b9b28
git <18501615527@163.com>
2019-04-14 20:31:02 +0800

    '第一次提交'

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Author:孙纪雷
+ * @Description:
+ * @DATE: 20:27  2019/4/14
+ * @Modified by:Administrator
+ **/
+public class Test {
+    public static void main(String[] args){
+        System.out.println("this is my first project");
+    }
+}
